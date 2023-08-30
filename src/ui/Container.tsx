a4d844475ce158d24099f28753482a46c0c88bb7
Luncert <2725115515@qq.com>
2023-08-30 09:27:12 +0800

    working on drag

--- src/ui/Container.tsx
+++ src/ui/Container.tsx
@@ -1,5 +1,5 @@
 
-import { PropsWithChildren, DOMAttributes, HTMLAttributes } from 'react';
+import { createElement, PropsWithChildren, HTMLAttributes } from 'react';
 import { conditionalString, names } from './utils';
 import styles from './Container.module.scss';
 
@@ -7,16 +7,27 @@ interface ContainerProps extends HTMLAttributes<HTMLElement> {
   blur?: boolean;
   radius?: boolean;
   shadow?: boolean;
+  defaultMargin?: boolean;
+  defaultPadding?: boolean;
 }
 
 export default function Card(props: PropsWithChildren<ContainerProps>) {
-  return (
-    <div className={names(
+  const p = {
+    ...props,
+    className: names(
       props.className || '',
       styles.container,
       conditionalString(blur, styles.blur),
       conditionalString(props.radius, styles.radius),
       conditionalString(props.shadow, styles.shadow),
-    )}>{props.children}</div>
-  )
+      conditionalString(props.defaultMargin != false, styles.defaultMargin),
+      conditionalString(props.defaultPadding != false, styles.defaultPadding),
+    )
+  };
+  delete p['blur'];
+  delete p['radius'];
+  delete p['shadow'];
+  delete p['defaultMargin'];
+  delete p['defaultPadding'];
+  return createElement('div', p, props.children);
 }
