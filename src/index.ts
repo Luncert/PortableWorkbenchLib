7e6d39d55f4fb4061f7691746c71e44e7eae6c46
Luncert <2725115515@qq.com>
2023-09-02 10:21:45 +0800

    .

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { default as Animator } from './components/Animator';
 export { default as Container } from './components/Container';
 export { default as Divider } from './components/Divider';
 export { default as List } from './components/List';
+export * as utils from './components/utils';
