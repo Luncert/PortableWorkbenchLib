df962d8c276307e0265bb56e1bc3fd012b3bd7ea
Luncert <2725115515@qq.com>
2023-09-02 11:00:36 +0800

    v1.0.9

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 
 export { default as AnimatedElement } from './components/AnimatedElement';
-export { default as Animator } from './components/Animator';
+export { default as Animator, animate } from './components/Animator';
 export { default as Container } from './components/Container';
 export { default as Divider } from './components/Divider';
-export { default as List } from './components/List';
+export { default as List, ListItem } from './components/List';
 export { names, check, conditionalRun, conditionalString, conditionalValue, compareDateString } from './components/utils';
